5a9cb387a9a025fe9b78719f5215618fc55c7eaf
Amedishh <ahmad.oumari@elev.ga.ntig.se>
2026-03-05 16:48:54 +0100

    Fixat klart skelettet av hemsidan
    Nu ska jag börja med login

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     setup_development_features(self)
 
     # Funktion för att prata med databasen
-    # Exempel på användning: db.execute('SELECT * FROM fruits')
+    # Exempel på användning: db.execute('SELECT * FROM bloggs')
     def db
       return @db if @db
       @db = SQLite3::Database.new(DB_PATH)
@@ -40,6 +40,34 @@ class App < Sinatra::Base
       rubrik = params["heading"]
       beskrivning = params["description"]
       db.execute("INSERT INTO bloggs (heading, description) VALUES (?,?)", [rubrik, beskrivning])
+      redirect("bloggs")
+    end
+
+    # Routen visar all info (från databasen) om en blogg
+    get '/bloggs/:id' do | id |
+      @blogg = db.execute('SELECT * FROM bloggs where id=?', id).first
+      p @blogg
+      erb(:"bloggs/show")
+    end
+   
+    # Routen tar bort bloggen med id
+    post '/bloggs/:id/delete' do | id |
+      db.execute("DELETE FROM bloggs WHERE id=?", id)
+      redirect"/bloggs"
+    end
+    
+    # Routen visar ett formulär på edit.erb för att ändra bloggen med id
+    get '/bloggs/:id/edit' do | id |
+      @blogg = db.execute('SELECT * FROM bloggs where id=?', id).first
+      erb(:"bloggs/edit")
+    end
+
+    # Routen sparar ändringarna från formuläret
+    post "/bloggs/:id/update" do | id |
+      u_rubrik = params["heading"]
+      u_beskrivning = params["description"]
+      p u_beskrivning
+      db.execute("UPDATE bloggs SET heading =?, description =? WHERE id =?", [u_rubrik, u_beskrivning, id])
       redirect("bloggs")
     end
 
